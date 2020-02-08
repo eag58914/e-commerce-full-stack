e7108dd5f59a049b41c4be2d441e1ccc39946408
Elco Garcia <garciaelco18@gmail.com>
2020-02-08 15:43:30 -0600

    setting up database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(
+	process.env.DATABASE_URL,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true
+	},
+	(err) => {
+		throw err;
+	}
+);
 
 const db = mongoose.connection;
 
