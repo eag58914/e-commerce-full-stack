30362e6bc01630ad19dbc2e95b59edd4a5ad6191
Elco Garcia <garciaelco18@gmail.com>
2020-02-26 15:54:28 -0600

    initial setup for tool bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Toolbar from '../src/components/Toolbar/Toolbar';
 
 function App() {
 	return (
 		<div>
-			<h1>test</h1>
+			<Toolbar />
 		</div>
 	);
 }
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Toolbar.module.css';
+const toolbar = (props) => (
+	<header>
+		<nav>
+			<div />
+			<div>
+				<a href="/"> The Logo</a>
+				<div>
+					<ul>
+						<li>
+							<a href="/">Placeholder</a>
+						</li>
+						<li>
+							<a href="/">Placeholder</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+);
+
+export default toolbar;
